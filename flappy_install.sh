b088adbac51f9448fa99a67c3a4a05f9f284e07e
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2020-09-02 22:05:32 -0400

    add to retropie ports

--- flappy_install.sh
+++ flappy_install.sh
@@ -3,9 +3,14 @@ cd
 echo "Flappy Bird script started!"
 echo "Installing Dependencies"
 echo
-sudo apt install --assume-yes libsdl2-2.0-0 libsdl2-image-2.0-0 libsdl2-mixer-2.0-0 libc6 libpng16-16 libjpeg-turbo8 libtiff5 libwebp6 libmodplug1 libfluidsynth1 libvorbisfile3 libflac8 libmad0 zlib1g liblzma5 libjbig0 libstdc++6 libglib2.0-0 libjack-jackd2-0 libasound2 libpulse0 libsndfile1 libreadline7 libvorbis0a libogg0 libgcc1 libpcre3 libc6 libpulse0 libvorbisenc2 libtinfo5 libdbus-1-3 libxcb1 libsystemd0 libwrap0 libasyncns0 libxau6 libxdmcp6 liblz4-1 libgcrypt20 libc6 libbsd0 libgpg-error0
+sudo apt install --assume-yes subversion libsdl2-2.0-0 libsdl2-image-2.0-0 libsdl2-mixer-2.0-0 libc6 libpng16-16 libjpeg-turbo8 libtiff5 libwebp6 libmodplug1 libfluidsynth1 libvorbisfile3 libflac8 libmad0 zlib1g liblzma5 libjbig0 libstdc++6 libglib2.0-0 libjack-jackd2-0 libasound2 libpulse0 libsndfile1 libreadline7 libvorbis0a libogg0 libgcc1 libpcre3 libc6 libpulse0 libvorbisenc2 libtinfo5 libdbus-1-3 libxcb1 libsystemd0 libwrap0 libasyncns0 libxau6 libxdmcp6 liblz4-1 libgcrypt20 libc6 libbsd0 libgpg-error0
 echo
 echo "Downloading the game"
+svn export https://github.com/cobalt2727/L4T-Megascript/trunk/assets/FlappyBird
+cd FlappyBird
+mv flappy.sh -t /$HOME/RetroPie/roms/ports
+cd
+rm -r FlappyBird
 wget https://github.com/theofficialgman/flappy/archive/master.zip
 unzip master.zip
 rm master.zip
